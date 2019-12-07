3b5da83fefc379694563d628228ad29bee5be1e0
Christian Escobar <crs.escobar@gmail.com>
2019-12-06 22:09:07 -0800

    implement classes

--- Letter.js
+++ Letter.js
@@ -1,26 +1,29 @@
-function Letter(letter) {
-	this.letter = letter;
-	this.hasBeenGuessed = false;
-	this.toString = function() {
+const isGuessCorrect = (userGuess, letter) => {
+	return userGuess.toLowerCase() === letter.toLowerCase();
+};
+
+class Letter {
+	constructor(letter) {
+		this.letter = letter;
+		this.hasBeenGuessed = false;
+	}
+
+	toString() {
 		// Displays the letter or an underscore based on if it has been guessed
-		if(this.hasBeenGuessed === true) {
-			return this.letter;
-		} else {
-			return '_';
-		}
-	};
-	this.guess = function(userGuess) {
+		const displayChar = this.hasBeenGuessed ? this.letter : '_';
+		return displayChar;
+	}
+
+	guess(userGuess) {
 		// Checks if a user's guess is a letter in the current word
-		if(this.hasBeenGuessed === true) {
-			return;
-		} else {
-			if(this.letter.toLowerCase() === userGuess.toLowerCase()) {
+		if (!this.hasBeenGuessed) {
+			if (isGuessCorrect(userGuess, this.letter)) {
 				this.hasBeenGuessed = true;
 			} else {
 				this.hasBeenGuessed = false;
 			}
 		}
-	};
+	}
 }
 
 module.exports = Letter;
