77a58fe8044a60c8a3da02cf6a0845ee1c2dfe58
Christian Escobar <crs.escobar@gmail.com>
2019-12-06 19:23:32 -0800

    linter changes

--- index.js
+++ index.js
@@ -1,48 +1,50 @@
-const word = require("./Word");
-const inquirer = require("inquirer");
-const chalk = require("chalk");
-const figlet = require("figlet");
+const word = require('./Word');
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+const figlet = require('figlet');
 
-const characterNames = ["Mario",
-			"Lara Croft",
-			"Samus Aran",
-			"Link",
-			"Master Chief",
-			"Kratos",
-			"Gordon Freeman",
-			"Nathan Drake",
-			"Luigi",
-			"Pikachu",
-			"Sephiroth",
-			"Glados",
-			"Jill Valentine",
-			"Marcus Fenix",
-			"Cloud Strife",
-			"Princess Zelda",
-			"Niko Bellic",
-			"Yoshi",
-			"Ganon",
-			"Subzero",
-			"Ryu Hayabusa",
-			"Fox McCloud",
-			"Toad",
-			"Donkey Kong",
-			"Sonic The Hedgehog",
-			"Crash Bandicoot",
-			"Earthworm Jim",
-			"Rayman",
-			"Diddy Kong",
-			"Bowser",
-			"Megaman",
-			"Max Payne",
-			"Pacman",
-			"Duke Nukem"];
+const characterNames = [
+	'Mario',
+	'Lara Croft',
+	'Samus Aran',
+	'Link',
+	'Master Chief',
+	'Kratos',
+	'Gordon Freeman',
+	'Nathan Drake',
+	'Luigi',
+	'Pikachu',
+	'Sephiroth',
+	'Glados',
+	'Jill Valentine',
+	'Marcus Fenix',
+	'Cloud Strife',
+	'Princess Zelda',
+	'Niko Bellic',
+	'Yoshi',
+	'Ganon',
+	'Subzero',
+	'Ryu Hayabusa',
+	'Fox McCloud',
+	'Toad',
+	'Donkey Kong',
+	'Sonic The Hedgehog',
+	'Crash Bandicoot',
+	'Earthworm Jim',
+	'Rayman',
+	'Diddy Kong',
+	'Bowser',
+	'Megaman',
+	'Max Payne',
+	'Pacman',
+	'Duke Nukem'
+];
 
 const maxGuessesAllowed = 10;
 
 // Start the game
 // IIFE function
-(function () {
+(function() {
 	displayTitle();
 })();
 
@@ -56,49 +58,54 @@ function initializeGame() {
 function playGame(totalGuesses, theWord) {
 	theWord.displayWord();
 
-	if(theWord.hasWordBeenGuessed()) {
+	if (theWord.hasWordBeenGuessed()) {
 		displayWinOrLose(true);
 
 		return;
 	}
 
-	if(totalGuesses < maxGuessesAllowed) {
+	if (totalGuesses < maxGuessesAllowed) {
 		displayHUD(totalGuesses, theWord);
 
-		inquirer.prompt([
+		inquirer
+			.prompt([
 				// User prompts go here
 				{
-					type: "input",
-					name: "userInput",
+					type: 'input',
+					name: 'userInput',
 					message: "What's your guess?",
 					validate: function(input) {
 						var pass = input.match(/^[a-zA-Z]/i);
 
-						if(pass) {
-							if(input.length > 1) {
-								return "Only single letters are allowed.";
-							} else if(theWord.letterHasBeenUsed(input) === false) {
+						if (pass) {
+							if (input.length > 1) {
+								return 'Only single letters are allowed.';
+							} else if (theWord.letterHasBeenUsed(input) === false) {
 								return true;
 							} else {
-								return "The letter '" + input + "' has already been used.  Try another one.";
+								return (
+									"The letter '" +
+									input +
+									"' has already been used.  Try another one."
+								);
 							}
 						}
 
-						return "Invalid character entered, only letters are valid.";
+						return 'Invalid character entered, only letters are valid.';
 					}
 				}
 			])
-		.then((answers) => {
-			totalGuesses++;
+			.then(answers => {
+				totalGuesses++;
 
-			theWord.takeGuess(answers.userInput);
+				theWord.takeGuess(answers.userInput);
 
-			// Store this guess
-			theWord.lettersGuessed.push(answers.userInput);
+				// Store this guess
+				theWord.lettersGuessed.push(answers.userInput);
 
-			// Recursive call
-			playGame(totalGuesses, theWord);
-		});
+				// Recursive call
+				playGame(totalGuesses, theWord);
+			});
 	} else {
 		displayWinOrLose(false);
 	}
@@ -111,84 +118,93 @@ function selectRandomCharacterName() {
 
 // Displays information to the user
 function displayHUD(totalGuesses, theWord) {
-	console.log(chalk.whiteBright("\nThis is guess #"), chalk.yellowBright(totalGuesses + 1));
-	console.log(chalk.whiteBright("Current guesses: "), chalk.yellowBright(theWord.lettersGuessed));
-	console.log(chalk.whiteBright("Number of Guesses Remaining: "), chalk.yellowBright(maxGuessesAllowed - totalGuesses, "\n"));
+	console.log(
+		chalk.whiteBright('\nThis is guess #'),
+		chalk.yellowBright(totalGuesses + 1)
+	);
+	console.log(
+		chalk.whiteBright('Current guesses: '),
+		chalk.yellowBright(theWord.lettersGuessed)
+	);
+	console.log(
+		chalk.whiteBright('Number of Guesses Remaining: '),
+		chalk.yellowBright(maxGuessesAllowed - totalGuesses, '\n')
+	);
 }
 
 // Display win or lose
 function displayWinOrLose(isWinner) {
-	if(isWinner) {
-		figlet("You Win!", function(err, data) {
-	    	if(err) {
-	    		throw err;
-	    	}
+	if (isWinner) {
+		figlet('You Win!', function(err, data) {
+			if (err) {
+				throw err;
+			}
 
-	    	console.log(data);
+			console.log(data);
 
-	    	playAgainPrompt();
-	   });
+			playAgainPrompt();
+		});
 	} else {
-		figlet("You Lose!", function(err, data) {
-	    	if(err) {
-	    		throw err;
-	    	}
+		figlet('You Lose!', function(err, data) {
+			if (err) {
+				throw err;
+			}
 
-	    	console.log(data);
+			console.log(data);
 
-	    	playAgainPrompt();
-	   });
+			playAgainPrompt();
+		});
 	}
 }
 
 // Display ascii art title
 function displayTitle(titleText) {
-	figlet("HANGMAN", function(err, data) {
-    if (err) {
-        throw err;
-    }
-    console.log(data);
+	figlet('HANGMAN', function(err, data) {
+		if (err) {
+			throw err;
+		}
+		console.log(data);
 
-    figlet("Video Game", function(err, data) {
-    	if(err) {
-    		throw err;
-    	}
+		figlet('Video Game', function(err, data) {
+			if (err) {
+				throw err;
+			}
 
-    	console.log(data);
+			console.log(data);
 
-    	figlet("Characters", function(err, data) {
-	    	if(err) {
-	    		throw err;
-	    	}
+			figlet('Characters', function(err, data) {
+				if (err) {
+					throw err;
+				}
 
-	    	console.log(data);
+				console.log(data);
 
-	    	initializeGame();
-	    });
-    });
+				initializeGame();
+			});
+		});
 	});
 }
 
 // Displays play again prompt
 function playAgainPrompt() {
-	inquirer.prompt([
+	inquirer
+		.prompt([
 			// User prompts go here
 			{
-				type: "confirm",
-				name: "playAgain",
-				message: "Would you like to play again?",
-				default: "Yes"
+				type: 'confirm',
+				name: 'playAgain',
+				message: 'Would you like to play again?',
+				default: 'Yes'
 			}
 		])
-	.then((answers) => {
-		if(answers.playAgain) {
-			initializeGame();
-		} else {
-			console.log(chalk.greenBright("OK, maybe next time."));
-		}
-	});
+		.then(answers => {
+			if (answers.playAgain) {
+				initializeGame();
+			} else {
+				console.log(chalk.greenBright('OK, maybe next time.'));
+			}
+		});
 }
-
 
 //====================================//
 // Test function section
@@ -204,22 +220,22 @@ function testWordGeneration() {
 }
 
 function testPrintWord(printThisWord) {
-	printThisWord.word.map((ltr) => {
+	printThisWord.word.map(ltr => {
 		console.log(ltr.letter, ' ==> ', ltr.hasBeenGuessed);
 	});
 }
 
 function testChalk() {
-	console.log(chalk.blue("Hello World!"));
+	console.log(chalk.blue('Hello World!'));
 }
 
 function testFiglet() {
 	figlet('Hello World!!', function(err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    }
-    console.log(data)
+		if (err) {
+			console.log('Something went wrong...');
+			console.dir(err);
+			return;
+		}
+		console.log(data);
 	});
 }
